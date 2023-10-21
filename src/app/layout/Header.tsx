db0ca90bb3e35f6e22826d66de3af226fda4df32
AhmedAdel1992 <ahmedadel.0792@gmail.com>
2023-10-21 15:01:02 +0300

    Basket Entity

--- src/app/layout/Header.tsx
+++ src/app/layout/Header.tsx
@@ -11,7 +11,8 @@ import {
   Box,
 } from "@mui/material";
 
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+import { useStoreContext } from "../context/StoreContext";
 
 const midLinks = [
   { title: "catalog", path: "/catalog" },
@@ -42,6 +43,9 @@ interface Props {
 }
 
 export default function Header({ darkMode, handleThemeChange }: Props) {
+  const { basket } = useStoreContext();
+  const itemCount = basket?.items.reduce((sum, item) => sum + item.quantity, 0);
+
   return (
     <AppBar position="static" sx={{ mb: 4 }}>
       <Toolbar
@@ -67,8 +71,15 @@ export default function Header({ darkMode, handleThemeChange }: Props) {
         </List>
 
         <Box display={"flex"} alignItems={"center"}>
-          <IconButton size="large" edge="start" color="inherit" sx={{ mr: 2 }}>
-            <Badge badgeContent="4" color="secondary">
+          <IconButton
+            component={Link}
+            to="/basket"
+            size="large"
+            edge="start"
+            color="inherit"
+            sx={{ mr: 2 }}
+          >
+            <Badge badgeContent={itemCount} color="secondary">
               <ShoppingCart />
             </Badge>
           </IconButton>
--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,14 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import {RouterProvider} from "react-router-dom"
+import { RouterProvider } from "react-router-dom";
 import { router } from "./app/router/Routes.tsx";
-
+import { StoreProvider } from "./app/context/StoreContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <StoreProvider>
+      <RouterProvider router={router} />
+    </StoreProvider>
   </React.StrictMode>,
 );
